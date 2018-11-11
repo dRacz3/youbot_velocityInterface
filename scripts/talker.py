f38e02b3a3e36e732268e2c08b52a59d81585c32
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-11-11 12:52:47 +0100

    minor upgrade to talker

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,44 +1,25 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
+import sys
+import pandas as pd
 
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+
+class velocity_container():
+    def __init__(self,df):
+        self.data = df
+        self.length = len(self.data)
+        self.index = 0
+
+    def getNext(self):
+        vx = self.data.iloc[self.index]['vx']
+        vy = self.data.iloc[self.index]['vy']
+        vomega = self.data.iloc[self.index]['omega']
+        self.index += 1
+        return vx,vy,vomega
+
 
 def talker():
     pub = rospy.Publisher('v_cmd', Twist, queue_size=10)
@@ -51,11 +32,11 @@ def talker():
         th = 0.1
         turn = 1
         twist = Twist()
-        twist.linear.x = x*speed 
-        twist.linear.y = y*speed 
+        twist.linear.x = x*speed
+        twist.linear.y = y*speed
         twist.linear.z = 0
 
-        twist.angular.x = 0 
+        twist.angular.x = 0
         twist.angular.y = 0
         twist.angular.z = th*turn
         pub.publish(twist)
@@ -65,6 +46,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
+        arguments = sys.argv[1:]
         talker()
     except rospy.ROSInterruptException:
         pass

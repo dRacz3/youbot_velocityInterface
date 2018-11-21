6276a6e343e39794afb7e9e596bad0b091651a1a
youbot <daniel.racz@irob.uni-obuda.hu>
2018-11-21 15:05:20 +0100

    Add progress bar info instead of spamming to talker

--- scripts/talker.py
+++ scripts/talker.py
@@ -7,6 +7,26 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 40, fill = '#'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 
 """
 This class acts as a container for the cartesian velocities.
@@ -18,26 +38,26 @@ class velocity_container():
         self.data = df.to_dict()
         self.length = len(df)
         self.index = 0
-        print('Velocity container initialized, data length is : {0}'.format(self.length))
 
     def __iter__(self):
         return self
 
     def next(self):
         if self.index >= self.length:
-            raise StopIteration
+			print("Execution finsihed!")
+			raise StopIteration
         else:
             vx = self.data['vx'][self.index]
             vy = self.data['vy'][self.index]
             vomega = self.data['omega'][self.index]
             self.index+=1
-            print 'next called with :{0},{1},{2}'.format(vx,vy,vomega)
+            printProgressBar(self.index, self.length, suffix = 'vx:{0:.3f}|vy:{1:.3f}|omega{2:.3f}'.format(float(vx),float(vy),float(vomega)))
             return vx,vy,vomega
 
 
 def talker(velocity_data):
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('talker', anonymous=True, log_level=rospy.WARN)
     rate = rospy.Rate(10) # 10hz
 
     for vx,vy,omega in velocity_data:
